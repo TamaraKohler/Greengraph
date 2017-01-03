b569f01b9ea6c7b791ce0df38f8a177613852e96
Tamara Kohler <tamara.kohler.16@ucl.ac.uk>
2017-01-03 19:00:06 +0000

    Added 'help' information to command.py file

--- greengraph/command.py
+++ greengraph/command.py
@@ -5,10 +5,10 @@ from matplotlib import pyplot as plt
 def process():
     parser = ArgumentParser(description = "Determine starting and ending locations")
     
-    parser.add_argument('--begin', '-b')
-    parser.add_argument('--end', '-e')
-    parser.add_argument('--steps', '-s')
-    parser.add_argument('--out', '-o')
+    parser.add_argument('--begin', '-b', help = 'The starting location for the map.')
+    parser.add_argument('--end', '-e', help = 'The end location for the map.')
+    parser.add_argument('--steps', '-s', help = 'The number of steps you would like to use in the calculation')
+    parser.add_argument('--out', '-o', help = 'Location to save the graph to')
     
     arguments = parser.parse_args()
     
