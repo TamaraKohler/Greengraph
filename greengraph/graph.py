959fc6ec0c92a83d4a7177567a07e31ba20c9f1d
Tamara Kohler <tamara.kohler.16@ucl.ac.uk>
2017-01-04 16:38:24 +0000

    Type check for the 'step' input added to the Greengraph class

--- greengraph/graph.py
+++ greengraph/graph.py
@@ -3,6 +3,13 @@ import numpy as np
 import geopy
 from .map import Map
 
+def is_number(s):    #Function checks if string has a representation as a float.
+            try:
+                float(s)
+                return True
+            except ValueError:
+                return False
+
 class Greengraph(object):
     def __init__(self, start, end):
         self.start=start
@@ -17,11 +24,23 @@ class Greengraph(object):
         return self.geocoder.geocode(place, exactly_one=False)[0][1]
     
     def location_sequence(self, start,end,steps):
-      lats = np.linspace(start[0], end[0], steps)
-      longs = np.linspace(start[1],end[1], steps)
-      return np.vstack([lats, longs]).transpose()
+        if not is_number(steps):
+            raise TypeError('Number of steps must be a number')
+        if float(steps) != int(float(steps)):
+            raise TypeError('Number of steps must be an integer')
+        if int(steps) <= 0:
+            raise ValueError('Number of steps must be positive')
+        lats = np.linspace(start[0], end[0], steps)
+        longs = np.linspace(start[1],end[1], steps)
+        return np.vstack([lats, longs]).transpose()
 
     def green_between(self, steps):
+        if not is_number(steps):
+            raise TypeError('Number of steps must be a number')
+        if float(steps) != int(float(steps)):
+            raise TypeError('Number of steps must be an integer')
+        if int(steps) <= 0:
+            raise ValueError('Number of steps must be positive')
         return [Map(*location).count_green()
                 for location in self.location_sequence(
                     self.geolocate(self.start), 
