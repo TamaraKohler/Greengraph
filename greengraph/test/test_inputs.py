946637606769d9e4fd2f3732a8f65e754d5fddaa
Tamara Kohler <tamara.kohler.16@ucl.ac.uk>
2017-01-04 16:39:42 +0000

    Tests to check input type of 'steps' added to the test_inputs.py file

--- greengraph/test/test_inputs.py
+++ greengraph/test/test_inputs.py
@@ -14,3 +14,23 @@ def test_end_type():
 def test_for_false_positive_location_type():
     with assert_raises(TypeError):
         assert Greengraph('London', 'Oxford')
+        
+def test_steps_type_1():
+    with assert_raises(TypeError):
+        assert Greengraph('London', 'Oxford').green_between('green')
+
+def test_steps_type_2():
+    with assert_raises(TypeError):
+        assert Greengraph('London', 'Oxford').green_between('3.5')
+        
+def test_steps_value():
+    with assert_raises(ValueError):
+        assert Greengraph('London', 'Oxford').green_between('-3')
+        
+def test_for_false_positive_steps_type():
+    with assert_raises(TypeError):
+        assert Greengraph('London', 'Oxford').green_between('3')
+        
+def test_for_false_positive_steps_value():
+    with assert_raises(ValueError):
+        assert Greengraph('London', 'Oxford').green_between('3')
