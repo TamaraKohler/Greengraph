7fbd6657df000e51196476cf7d03ad481b0cad94
Tamara Kohler <tamara.kohler.16@ucl.ac.uk>
2017-01-05 19:03:34 +0000

    Added a test to test how the Map class is building a map, and saved a london_map.png in fixtures so test doesn't interact with internet.

---
+++ greengraph/test/test_map.py
@@ -0,0 +1,27 @@
+
+from unittest.mock import Mock, patch
+from greengraph import Greengraph
+from greengraph.map import Map
+import requests
+import os
+import numpy.testing as npt
+from matplotlib import image as img
+import numpy
+from io import BytesIO
+from nose.tools import assert_equals
+
+
+lat = 51.5073509
+long = -0.1277583
+
+def test_build_map():
+    mock_image = open(os.path.join(os.path.dirname(__file__),
+                                   'fixtures/london_map.png'), 'rb')
+    with patch('requests.get', return_value=Mock(content=mock_image.read())) as mock_get:
+        test_map = Map(lat, long)
+        mock_get.assert_called_with(
+            'http://maps.googleapis.com/maps/api/staticmap?',
+            params=dict(size='400x400', zoom=10, center='51.5073509,-0.1277583',
+                        style='feature:all|element:labels|visibility:off', sensor='false', maptype='satellite')
+        )
+        
